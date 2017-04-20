dd58d35f975c1f0abf747cd61792f812c0ac69c0
Meili Wang <meili.wang@appdynamics.com>
2017-04-20 00:38:59 -0700

    Creating more metrics data:    one revenue metrics, 5 Internal Error metrics, 9 CPU metrics

--- util/src/main/java/util/SimpleGenerator.java
+++ util/src/main/java/util/SimpleGenerator.java
@@ -4,7 +4,6 @@ package util;
 
 import java.sql.*;
 import java.text.ParseException;
-import java.util.Arrays;
 import java.util.Random;
 
 public class SimpleGenerator {
@@ -20,6 +19,98 @@ public class SimpleGenerator {
             7, 6, 5,4,4,
             3, 3, 2, 1,1, 1};
 
+    static int[] dropValues = {
+            1, 2, 2, 2, 3,
+            3, 4, 5, 7, 9,
+            12, 15, 18, 22, 27,
+            30, 35, 40, 46, 80,
+            46, 43, 35, 30, 27,
+            22, 18,15, 12, 9,
+            7, 6, 5,4,4,
+            3, 3, 2, 1,1, 1};
+
+    static int[] spikeCPUValues_0 = {
+            1, 2, 2, 2, 3,
+            3, 4, 5, 7, 9,
+            12, 15, 18, 22, 27,
+            30, 35, 40, 46, 78,
+            46, 43, 35, 30, 27,
+            22, 18,15, 12, 9,
+            7, 6, 5,4,4,
+            3, 3, 2, 1,1, 1};
+
+    static int[] spikeCPUValues_1 = {
+            1, 1, 1, 2, 2,
+            2, 3, 4, 6, 7,
+            10, 14, 17, 20, 25,
+            28, 32, 38, 44, 76,
+            44, 41, 33, 28, 25,
+            20, 16, 15, 10, 7,
+            6, 5, 4,3,3,
+            3, 2, 1, 1,1, 1};
+    static int[] spikeCPUValues_2 = {
+            1, 1, 2, 2, 3,
+            3, 3, 3, 4, 5,
+            11, 15, 18, 23, 26,
+            29, 33, 37, 45, 75,
+            45, 43, 32, 29, 24,
+            21, 17, 16, 11, 8,
+            7, 6, 5,3,3,
+            3, 2, 2, 1,1, 1};
+    static int[] spikeCPUValues_3 = {
+            1, 1, 1, 2, 4,
+            5, 6, 7, 8, 10,
+            12, 13, 19, 20, 25,
+            31, 33, 40, 46, 76,
+            47, 44, 35, 31, 26,
+            22, 18,15, 12, 9,
+            7, 6, 5,4,4,
+            3, 3, 2, 1,1, 1};
+    static int[] spikeCPUValues_4 = {
+            1, 2, 2, 2, 3,
+            3, 4, 5, 7, 9,
+            12, 15, 18, 22, 27,
+            30, 35, 43, 45, 76,
+            46, 43, 35, 30, 27,
+            22, 18,15, 12, 9,
+            7, 6, 5,4,4,
+            3, 3, 2, 1,1, 1};
+    static int[] spikeCPUValues_5 = {
+            1, 2, 2, 2, 3,
+            3, 4, 5, 7, 9,
+            12, 15, 18, 22, 27,
+            30, 35, 40, 46, 75,
+            46, 43, 35, 30, 27,
+            22, 18,15, 12, 9,
+            7, 6, 5,4,4,
+            3, 3, 2, 1,1, 1};
+    static int[] spikeCPUValues_6 = {
+            1, 2, 2, 2, 3,
+            3, 4, 5, 7, 9,
+            12, 15, 18, 22, 27,
+            30, 35, 40, 46, 74,
+            46, 43, 35, 30, 27,
+            22, 18,15, 12, 9,
+            7, 6, 5,4,4,
+            3, 3, 2, 1,1, 1};
+    static int[] spikeCPUValues_7 = {
+            1, 2, 2, 2, 3,
+            3, 4, 5, 7, 9,
+            12, 15, 18, 22, 27,
+            30, 35, 40, 46, 73,
+            46, 43, 35, 30, 27,
+            22, 18,15, 12, 9,
+            7, 6, 5,4,4,
+            3, 3, 2, 1,1, 1};
+    static int[][] spikeCPUValues = {
+            spikeCPUValues_0,
+            spikeCPUValues_1,
+            spikeCPUValues_2,
+            spikeCPUValues_3,
+            spikeCPUValues_4,
+            spikeCPUValues_5,
+            spikeCPUValues_6,
+            spikeCPUValues_7};
     static int total = 60 * 24 * 14;   // two week data, 1 entry per minute
     static int spikeStartPoints = 60 * 24 * 12; // spike start 13th day
     static int peakPoints = 41;   // 41 mins spike
@@ -42,8 +133,15 @@ public class SimpleGenerator {
             generator.setConnection();
             generator.dropTable();
             generator.createTable();
+
+            // create revenue data
+            generator.putRevenueData(startTime);
+            // insert internal error
+            generator.putInternalErrorData(startTime);
             // create cpu data
             generator.putCPUData(startTime);
+
+            generator.selectRows("SELECT * FROM metrics");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -55,16 +153,29 @@ public class SimpleGenerator {
         }
     }
 
-    // num = 41, 31, 21, 11;
+    private void putInternalErrorData(long startTime) throws SQLException {
+
+        String[] names = {
+                "Application Infrastructure Performance|shepherd|Custom Metrics|Synthetic|Shepherd|Sessions|Failed|By session status|INTERNAL_ERROR|Overall",
+                "Application Infrastructure Performance|shepherd|Custom Metrics|Synthetic|Shepherd|Sessions|Failed|By session status|INTERNAL_ERROR|By error code|FAILED_TO_START_SANDBOX|Overall",
+                "Application Infrastructure Performance|shepherd|Custom Metrics|Synthetic|Shepherd|Sessions|Failed|By session status|INTERNAL_ERROR|By error code|AGENT_FAILED_TO_POST_RESULT|Overall",
+                "Application Infrastructure Performance|shepherd|Custom Metrics|Synthetic|Shepherd|Sessions|Failed|By session status|INTERNAL_ERROR|By error code|EMPTY_HAR|Overall",
+                "Application Infrastructure Performance|shepherd|Custom Metrics|Synthetic|Shepherd|Sessions|Failed|By session status|INTERNAL_ERROR|By error code|FETCH_HAR_ERROR|Overall",
+        };
+        double[][] values = generateParentAndChildrenInternalDataWithSpike(names.length -1);
+        for (int i = 0; i < names.length; i++) {
+            insertMetricsData(startTime, values[i], names[i]);
+        }
+    }
+       // num = 41, 31, 21, 11;
     // TODO
     private int[] generateSpikeValues(int num, int low, int high) {
         int[]  res  = new int[num];
         return res;
     }
     private void putCPUData(long startTime) throws SQLException {
-        double[][] cPUs = generateParentAndChildrenDataWithSpike(numberOfChildren);
-        insertToCPUTotal(startTime, cPUs[0]);
         String[] names = {
+                "Application Infrastructure Performance|lemminghost|Hardware Resources|CPU|%Busy",
                 "Application Infrastructure Performance|lemminghost|Individual Nodes|ip-10-130-0-175|Hardware Resources|CPU|%Busy",
                 "Application Infrastructure Performance|lemminghost|Individual Nodes|ip-10-130-0-197|Hardware Resources|CPU|%Busy",
                 "Application Infrastructure Performance|lemminghost|Individual Nodes|ip-10-130-0-76|Hardware Resources|CPU|%Busy",
@@ -74,13 +185,18 @@ public class SimpleGenerator {
                 "Application Infrastructure Performance|lemminghost|Individual Nodes|ip-10-130-2-128|Hardware Resources|CPU|%Busy",
                 "Application Infrastructure Performance|lemminghost|Individual Nodes|ip-10-130-2-239|Hardware Resources|CPU|%Busy"
         };
-        for (int i = 1; i <= numberOfChildren; i ++) {
-            insertCPUDataForNode(startTime, cPUs[i], names[i-1]);
+        double[][] cPUs = generateParentAndChildrenCPUDataWithSpike(names.length -1);
+
+        for (int i = 0; i < names.length; i ++) {
+            insertMetricsData(startTime, cPUs[i], names[i]);
         }
-        selectRows("SELECT * FROM metrics");
 
     }
 
+    private void putRevenueData(long startTime) throws  SQLException {
+        double[] values = generateSingleLineDataWithDrop();
+        insertMetricsData(startTime, values, "Custom Metrics|Revenue|Overall");
+    }
     public void setConnection () throws Exception {
             Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection("jdbc:sqlite:demo.db");
@@ -102,14 +218,7 @@ public class SimpleGenerator {
         stmt.close();
     }
 
-    private void insertToCPUTotal(long startTime, double[] values) throws SQLException {
-        System.out.println(insertSql);
-        PreparedStatement stmt;
-        stmt = conn.prepareStatement(insertSql);
-        insertByBatch(startTime, values, stmt,"Application Infrastructure Performance|lemminghost|Hardware Resources|CPU|%Busy" );
-    }
-
-    private void insertCPUDataForNode(long startTime, double[] values, String name) throws SQLException {
+    private void insertMetricsData(long startTime, double[] values, String name) throws SQLException {
         PreparedStatement stmt;
         stmt = conn.prepareStatement(insertSql);
         insertByBatch(startTime, values, stmt, name);
@@ -136,7 +245,7 @@ public class SimpleGenerator {
             String name = rs.getString("metricpath");
             long timestamp = rs.getLong("timestamp");
             int  value = rs.getInt("value");
-            if (value > avg) {
+            if (count % 100 == 0) {
                 System.out.println("name = " + name + "; timestamp = " + timestamp + "; value = " + value);
             }
             count ++;
@@ -147,14 +256,14 @@ public class SimpleGenerator {
         stmt.close();
     }
 
-    private double[] generateSingleLineDataWithSpike() {
+    private double[] generateSingleLineCPUDataWithSpike(int[] sValues) {
         double[] res = new double[total];
         int bounce = 6;
         for (int i = 0; i< spikeStartPoints; i ++) {
             res[i] = random.nextInt(bounce) + avg - bounce;
         }
         for (int i = spikeStartPoints; i < spikeStartPoints + peakPoints; i ++) {
-            res [i] = spikeValues[i- spikeStartPoints] + avg;
+            res [i] = sValues[i- spikeStartPoints] + avg;
             //System.out.println(i +"==>" + res[i]);
         }
 
@@ -164,16 +273,40 @@ public class SimpleGenerator {
         return res;
     }
 
-    private double[][] generateParentAndChildrenData(int numOfChildren) {
-        double[][] groupData = new double[numOfChildren+1][total];
-        groupData[0] = generateSingleLineDataWithSpike();
-        for (int i = 0; i < total; i ++) {
-            double[] subs = distributeDataBetweenChildren(groupData[0][i], numOfChildren);
-            for (int j = 1 ; j <= numOfChildren; j ++) {
-                groupData[j][i]= subs[j-1];
-            }
+    private double[] generateSingleLineInternalErrorDataWithSpike() {
+        double normalValue=10;
+        double[] res = new double[total];
+        int bounce = 6;
+        for (int i = 0; i< spikeStartPoints; i ++) {
+            res[i] = random.nextInt(bounce) + normalValue - bounce;
         }
-        return groupData;
+        for (int i = spikeStartPoints; i < spikeStartPoints + peakPoints; i ++) {
+            res [i] = spikeValues[i- spikeStartPoints] * 20 + normalValue;
+            //System.out.println(i +"==>" + res[i]);
+        }
+
+        for (int i = spikeStartPoints + peakPoints; i < total ; i ++) {
+            res[i]= random.nextInt(bounce) + normalValue - bounce;
+        }
+        return res;
+    }
+
+    private double[] generateSingleLineDataWithDrop() {
+        double normalValue = 30000;
+        double[] res = new double[total];
+        int bounce = 5000;
+        for (int i = 0; i< spikeStartPoints; i ++) {
+            res[i] = random.nextInt(bounce) + normalValue - bounce;
+        }
+        for (int i = spikeStartPoints; i < spikeStartPoints + peakPoints; i ++) {
+            res [i] =  normalValue - dropValues[i- spikeStartPoints] * 200;
+            //System.out.println(i +"==>" + res[i]);
+        }
+
+        for (int i = spikeStartPoints + peakPoints; i < total ; i ++) {
+            res[i]= random.nextInt(bounce) + normalValue - bounce;
+        }
+        return res;
     }
 
     private double[] distributeDataBetweenChildren(double sum, int numOfChildren) {
@@ -194,9 +327,24 @@ public class SimpleGenerator {
         return res;
     }
 
-    private double[][] generateParentAndChildrenDataWithSpike(int numOfChildren) {
+    private double[][] generateParentAndChildrenCPUDataWithSpike(int numOfChildren) {
         double[][] groupData = new double[numOfChildren+1][total];
-        groupData[0] = generateSingleLineDataWithSpike();
+        for (int i = 1; i <= numOfChildren; i ++) {
+            groupData[i] = generateSingleLineCPUDataWithSpike(spikeCPUValues[i-1]);
+        }
+
+        for (int i = 0; i < total; i ++) {
+            groupData[0][i] = 0;
+            for (int j = 1; j<= numOfChildren ; j ++) {
+                groupData[0][i] += groupData[j][i];
+            }
+        }
+
+        return groupData;
+    }
+    private double[][] generateParentAndChildrenInternalDataWithSpike(int numOfChildren) {
+        double[][] groupData = new double[numOfChildren+1][total];
+        groupData[0] = generateSingleLineInternalErrorDataWithSpike();
         for (int i = 0; i < spikeStartPoints; i ++) {
             double[] subs = distributeDataBetweenChildren(groupData[0][i], numOfChildren);
             for (int j = 1 ; j <= numOfChildren; j ++) {
